4e4d1df740cb5d0924a356c7a480fc85213a6295
Hoang Trung Kien <htrungkien1806@gmail.com>
2025-05-07 14:48:05 +0700

    view, text, styles

--- App.tsx
+++ App.tsx
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.header}>LolaKien</Text>
+        <Text style={styles.parrent}>BLA BlA bla </Text>
+        <Text style={styles.child}>aaaaaaaaaaaaa aa </Text>
+      </View>
+      <Text style={styles.helo1}>Helo word Kiennnnn!</Text>
       <Text>Helo word LOLA Kiennnnn!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,4 +22,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  helo1: {
+    color: "aqua", fontSize: 40,
+    borderColor: "red",
+    borderWidth: 2,
+    padding: 10
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  parrent: {
+    fontSize: 40,
+    color: "green"
+  },
+  child: {
+    fontSize: 20,
+    color: "yellow"
+  }
 });
